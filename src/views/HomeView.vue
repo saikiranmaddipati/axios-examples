28557f4af86b7800a13ee34db6bbd3fdf0da0ad0
saikiranmaddipati <maddipatisaikiran24@gmail.com>
2022-09-25 21:16:01 +0530

    axios-examples

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <post-details></post-details>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostDetails from '../components/PostDetails.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    PostDetails
   }
 }
 </script>
